704df760d509d9d64eeb604f1721fac9c407f595
DenisMobile <danchic95@gmail.com>
2019-10-23 23:13:27 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnnounceComponent } from './announce/announce.component';
+import { StoreComponent } from './store/store.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { CinemaComponent } from './cinema/cinema.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'announce', component: AnnounceComponent },
+  { path: 'store', component: StoreComponent },
+  { path: 'cinema', component: CinemaComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnnounceComponent } from './announce/announce.component';
+import { StoreComponent } from './store/store.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { CinemaComponent } from './cinema/cinema.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnnounceComponent,
+    StoreComponent,
+    NotFoundComponent,
+    HomeComponent,
+    CinemaComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
